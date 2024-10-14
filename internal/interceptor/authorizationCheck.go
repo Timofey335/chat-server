a406dedd501bac4f27bf9111dae4706a5ab587dd
Timofey335 <timofeyvalerievich+git@gmail.com>
2024-10-14 17:48:43 +0300

    in progress

--- internal/interceptor/authorizationCheck.go
+++ internal/interceptor/authorizationCheck.go
@@ -1,3 +1,46 @@
 package interceptor
 
-func AuthorizarionCheck()
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+
+	descAccess "github.com/Timofey335/chat-server/pkg/access_v1"
+)
+
+var accessToken = flag.String("a", "", "access token")
+
+const servicePort = 50051
+
+func AuthorizarionCheck(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	flag.Parse()
+
+	md := metadata.New(map[string]string{"Authorization": "Bearer " + *accessToken})
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	conn, err := grpc.Dial(
+		fmt.Sprintf(":%d", servicePort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("failed to dial GRPC client: %v", err)
+	}
+
+	cl := descAccess.NewAccessV1Client(conn)
+
+	_, err = cl.Check(ctx, &descAccess.CheckRequest{
+		EndpointAddress: "/auth_v1.Auth_v1/GetUser",
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Access granted")
+
+	return handler(ctx, req)
+}
